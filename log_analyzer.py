601807e8bf8bed7d711b9b4cfe72a80052d67339
daniel-chang-github <81652137+daniel-chang-github@users.noreply.github.com>
2022-07-31 23:04:33 -0400

    Add files via upload

---
+++ log_analyzer.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from pprint import pprint
+
+path = 'mnt/airflow/logs/marketvol2'
+
+def analyze_file(**kwargs):
+    logfiles = Path(path).rglob('*.log')
+    error_count = 0
+    error_lines = []
+    for file in logfiles:
+        # print(file)
+        with open(file, 'r') as logfile:
+            for line in logfile:
+                if 'ERROR' in line:
+                    error_count += 1
+                    error_lines.append(str(file) + '\n' + line  )
+    pprint(error_count) 
+    pprint(error_lines)
+
+
+print(analyze_file())
